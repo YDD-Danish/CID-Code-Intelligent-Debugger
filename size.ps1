818106e1c200171cbe7e78866891d4d6e027ae87
YDD-Danish <dk1200002@gmail.com>
2026-08-29 20:03:29 +0530

    Delete size.ps1

--- size.ps1
+++
@@ -1,4 +0,0 @@
-'
-$size = (Get-ChildItem "$env:USERPROFILE\C.I.D" -Recurse -Force -Exclude "venv","__pycache__","*.db" -ErrorAction SilentlyContinue | Where-Object { $_.FullName -notmatch ''\\venv\\|\\__pycache__\\'' } | Measure-Object -Property Length -Sum).Sum
-"Project size: {0:N2} MB" -f ($size / 1MB)
-' | Out-File "$env:USERPROFILE\C.I.D\check_size.ps1"
